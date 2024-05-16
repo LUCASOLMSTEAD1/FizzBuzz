10ce20a49283f26d63d649d74f0f507d6b28b901
Lucas Olmstead <lucasolmstead@Lucass-MacBook-Pro.local>
2024-05-16 19:37:42 -0400

    finished task

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 0;
+        int i = 100;
+        while ( i > 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else {
+                i = i - 1;
+            } n += 1;
+        }
+        System.out.println(n);
+    }
 }
